aafb06e2a2633306bd99b85d9ce24597ad74bf0b
nastiachmel <nastiachmel@gmail.com>
2019-11-16 10:08:05 +0200

    add

---
+++ test2/js/main.js
@@ -0,0 +1,13 @@
+
+
+(function($){
+  $(function() {
+    $('.menu__icon').on('click', function() {
+      $(this).closest('.menu')
+        .toggleClass('menu_state_open');
+     
+    });
+      });
+})(jQuery);
+
+
